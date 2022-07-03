7bede55c9d53c9e85bfc59eeba841d1866fff542
Dobricean Ioan Dorian <dobriceanioandorian@Bucur-Sabaus-MacBook-Air.local>
2022-07-03 11:05:35 +0300

    border fix

--- src/Pages/Orare/custom-view-slot.jsx
+++ src/Pages/Orare/custom-view-slot.jsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { SchedulerViewSlot } from "@progress/kendo-react-scheduler";
 export const CustomViewSlot = (props) => {
   let customStyle = {};
-  console.log(props);
-  if (props.range.index === 0) {
+  console.log(props.index, props.range.index);
+  if (props.index === 0 || props.range.index === 0) {
     customStyle = {
       borderLeft: "1px solid black",
     };
